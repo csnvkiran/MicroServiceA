af19ae0c483504d7f71d7b16da50f7465094eaec
Chalancharla Sri Naga Venkata Kiran <csnvkiran@gmail.com>
2017-07-28 23:12:12 +0400

    Add project files.

---
+++ LocationServices/LocationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServiceA.LocationServices
+{
+    public class LocationService
+    {
+
+        public LocationService(double latitude, double longitude, int pid)
+        {
+
+            PersonID = pid;
+            PersonName = "Kiran Chalancharls";
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public int PersonID { get; }
+        public string PersonName { get; }
+        public double Latitude { get; }
+        public double Longitude { get; }
+
+    }
+ 
+}
---
+++ Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MicroServiceA.Extensions;
+using MicroServiceA.LocationServices;
+using Newtonsoft.Json;
+
+namespace MicroServiceA
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+
+                var latString = context.Request.Query["lat"].FirstOrDefault();
+                var longString = context.Request.Query["long"].FirstOrDefault();
+
+                var latitude = latString.TryParse();
+                var longitude = longString.TryParse();
+
+                if (latitude.HasValue && longitude.HasValue)
+                {
+                    var forecast = new LocationService(latitude.Value, longitude.Value, 10);
+                    var json = JsonConvert.SerializeObject(forecast, Formatting.Indented);
+                    context.Response.ContentType = "application/json; charset=utf-8";
+                    await context.Response.WriteAsync(json);
+                }
+                else
+                {
+
+                    await context.Response.WriteAsync($"Unable to get locaton of  lat: {latitude}, long: {longitude}");
+                }
+
+
+            });
+        }
+    }
+}
